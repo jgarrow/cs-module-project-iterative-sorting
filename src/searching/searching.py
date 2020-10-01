5a93706592c76aa00cfc79dff1d4dae865a75c1b
Janessa Garrow <janessa.garrow@gmail.com>
2020-09-30 21:27:26 -0600

    Complete binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -9,6 +9,19 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    lowest = 0
+    highest = len(arr) - 1
 
+    while lowest <= highest:
+        middle = lowest + (highest - lowest) // 2
+
+        print('middle: ', middle)
+
+        if arr[middle] == target:
+            return middle
+        elif arr[middle] > target:
+            highest = middle - 1 
+        elif arr[middle] < target:
+            lowest = middle + 1
 
     return -1  # not found
