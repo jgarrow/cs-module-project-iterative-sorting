eaf91bc903768818d62f7d32cdb141d7a4ec83fc
Janessa Garrow <janessa.garrow@gmail.com>
2020-09-30 20:37:09 -0600

    Complete bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,58 @@
 # TO-DO: Complete the selection_sort() function below
-def selection_sort(arr):
-    # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc)
-        # Your code here
+# def selection_sort(arr):
+#     # loop through n-1 elements
+#     for i in range(0, len(arr) - 1):
+#         cur_index = i
+#         smallest_index = cur_index
+#         # TO-DO: find next smallest element
+#         # (hint, can do in 3 loc)
+#         # Your code here
 
 
-        # TO-DO: swap
-        # Your code here
+#         # TO-DO: swap
+#         # Your code here
 
-    return arr
+#     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
     # Your code here
 
+    # if there are less than 2 elements, there's nothing to swap
+    if len(arr) < 2:
+        return arr
+
+    curr_index = 0
+    swapped = True
+
+    while swapped is True:
+        # reset the index to zero after going through the whole list
+        curr_index = 0
+
+        # will still finish this whole block of code
+        # if swapped is still False at the end, it will exit this while loop
+        swapped = False
+
+        # check if the curr_index and it's next increment both exist in the list
+        while curr_index < len(arr) and curr_index + 1  < len(arr):
+            
+            # if the next element is smaller than the current one, swap them
+            if arr[curr_index + 1] < arr[curr_index]:
+                # temporary variable to hold on to what the current next element is
+                curr_next = arr[curr_index + 1]
+
+                # set the next element to our current index's element
+                arr[curr_index + 1] = arr[curr_index]
+
+                # set the current index value to the next element that we held onto
+                arr[curr_index] = curr_next
+                swapped = True
+
+            # move onto the next pair
+            curr_index += 1
+
+    print('arr: ', arr)
 
     return arr
 
